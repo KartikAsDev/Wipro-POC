81c28273afebccbd45c92291276af694c17cd450
kartikasdev <kartikasdev@gmail.com>
2020-09-26 15:26:50 +0530

    movie-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Table from 'react-bootstrap/Table';
+import Modal from './Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      movieItems: [],
+      show: false,
+      currentMovie: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then(res => res.json())
+      .then(
+        (result) => {
+         this.setState({
+           movieItems: result,
+         })
+        },
+        (error) => {
+          console.log('Error', error);
+        }
+      )
+  }
+
+  showModal = (item) => {
+    this.setState({ 
+      show: true,
+      currentMovie: item,
+    });
+  }
+  
+  hideModal = () => {
+    this.setState({ show: false });
+  }
+
+  render() {
+    const items = this.state.movieItems;
+
+    const moviePoster = 'https://instagram.fknu1-1.fna.fbcdn.net/v/t51.2885-15/e35/15043434_155934458210613_6676873630565007360_n.jpg?_nc_ht=instagram.fknu1-1.fna.fbcdn.net&_nc_cat=109&_nc_ohc=jcrHn1-ptSMAX95R5PZ&_nc_tp=18&oh=d5da5c78a8645e8eb98b6dfaf296747c&oe=5F978ACC';
+    return (
+      <div className="App">
+        <header className="App-header">
+            Movies Table
+        </header>
+        <hr />
+        <Table striped bordered hover responsive>  
+          <thead>
+            <tr>
+              <th>The Movie Title</th>
+              <th>The Director’s Name</th>
+              <th>Movie’s Rating</th>
+              <th>Movie Poster</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map(item => (
+                <tr key={item.id}>
+                  <td onClick={() => this.showModal(item)}>{item.title}</td>
+                  <td>{item.director}</td>
+                  <td>{item.rt_score}</td>
+                  {/* <td>{item.url}</td> */}
+                  <td><img src={moviePoster} alt="Movie-Image" height='30' width='30'></img></td>
+                </tr>
+              ))}
+          </tbody>    
+        </Table>
+        <Modal show={this.state.show} handleClose={this.hideModal} currentMovie={this.state.currentMovie}>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
