52d9e91c5bad9a2b255240341cad084d4b91c157
kartikasdev <kartikasdev@gmail.com>
2020-12-31 13:22:53 +0530

    initial push

--- src/App.js
+++ src/App.js
@@ -1,74 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
+import { getSuggestions } from './mock-server'
 
 class App extends Component{
   constructor() {
     super();
     this.state = {
-      n: '',
+      suggestions: [],
+      inputValue: '',
     }
   }
 
+  handleClick(e) {
+    document.getElementById('suggestionModal').style.display = "block";
+    document.getElementById('suggestionBox').hidden = false;
+    let value = e.target.value;
+    getSuggestions(value).then(suggestions => {
+      this.setState({
+        suggestions: suggestions,
+        inputValue: value,
+      }) ;
 
-  componentDidUpdate(props, state) {
-    document.getElementById('chessBoard').innerHTML = "";
-    var chessBoard = document.getElementById('chessBoard');
-    for (var i = 0; i < this.state.n; ++i){
-      var row = document.createElement('div')
-      row.className = 'row';
+      const matchingItem = suggestions.find(item => {
+        return item.indexOf(value) !== -1;
+      });
 
-      if(this.state.n%2 === 0 && i % 2 === 0) {
-        row.style.flexDirection = 'row-reverse'; 
-      } 
-      row.style.height = `${400/this.state.n}px`;
-      for (var j = 0; j < this.state.n; ++j){
-        var column = document.createElement('div')
-        column.className = 'column';
-        if(this.state.n%2 !== 0 && i%2 === 0) {
-          column.style.backgroundColor = j % 2 === 0 ? 'white' : 'black';
-        } else if(this.state.n%2 !== 0 && i%2 !== 0) {
-          column.style.backgroundColor = j % 2 === 0 ? 'black' : 'white';
-        } else {
-          column.style.backgroundColor = j % 2 === 0 ? 'white' : 'black';
+      const listItems = document.getElementsByTagName('li');
+      for(let i =0; i < listItems.length; i++) {
+        if(listItems[i].outerText === matchingItem) {
+          listItems[i].setAttribute('style', 'background-color: #E0E0E0');
         }
-        column.style.width = `${400/this.state.n}px`;
-        column.style.height = `${400/this.state.n}px`;
-        column.id = `column${i}${j}`;
-        column.onclick = (ele) => {
-          console.log('ele inside', ele);
-          this.handleSelect(ele);
-          ele.tabIndex="0"
-          ele.target.focus();
-        }
-        row.appendChild(column)
       }
-      chessBoard.appendChild(row)
-    }   
-   }
+    });
+  }
 
-   handleValueOfN(ele) {
-     this.setState({
-       n: ele.target.value,
-     });
-   }
+  selectItem(e, data) {
+    document.getElementById('suggestionModal').style.display = "none";
+    e.stopPropagation();
+    e.preventDefault();
+    this.setState({
+      inputValue: data + " ",
+      suggestions: [],
+    });
+    document.getElementById('searchBox').focus();
+    document.getElementById('suggestionBox').hidden = true;
+  }
 
-   handleSelect(ele) {
-    ele.target.style.backgroundColor = 'yellow';
-   }
-
-   handleClick(ele) {
-    console.log('ele', ele);
-   }
+  onExitingInputBox(e) {
+    // document.getElementById('suggestionBox').hidden = true;
+  }
 
   render() {
+    const showSuggestions =  <ul id="suggestionBox">
+          {this.state.suggestions.map(item =>
+          <li className="suggestionItem"
+                key={item} 
+                onClick={(e) => this.selectItem(e, item)}>
+                {item}
+              </li>)}
+          </ul>
+
     return (
       <div className="app">
-        <div className="inputDiv">
-          <span> Enter Grid Size </span>
-          <input value={this.state.n} onChange={(ele) => this.handleValueOfN(ele)}></input>
-        </div>
-
-        <div id="chessBoard" className="chessBoard" onClick={(ele) => this.handleClick(ele)}>
+        <div >
+          <input name="search" 
+                type="text"
+                value={this.state.inputValue}
+                id="searchBox"
+                onChange={(e) => this.handleClick(e)}
+                onClick={(e) => this.handleClick(e)}>
+          </input>
+          <div id="suggestionModal">
+            {showSuggestions}
+          </div>
         </div>
       </div>
     );
